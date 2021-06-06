44df6c1bc040f7404d367f10f8d07858ef3ac6bc
Hakim Maghraoui <h.maghraoui@mcq-scan.com>
2021-06-06 16:16:29 +0200

    added price data to be read from db

--- Core/views.py
+++ Core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 import requests
 from dogefather_web.config import current_site_url
-from django.http import HttpResponseRedirect
-import json
+from Core.models import TokenData
 
 # Create your views here.
 HEADERS = {
@@ -26,16 +25,19 @@ def get_num_of_zeros(num):
 
 def home_view(request):
     # Get price and format it
-    pancake_price = requests.get('https://api.pancakeswap.info/api/tokens/0x3D29Aa78fB558F84112bbC48a84F371147A920C9',
-                         headers=HEADERS)
-    pancake_price = pancake_price.json()
-    pancake_price = pancake_price["data"]["price"]
+    dagefather_data = TokenData.objects.get(symbol='DOGEFATHER')
+
+    pancake_price = dagefather_data.current_price
     float_price = float(pancake_price)
     num_zeros = get_num_of_zeros(float_price)
     zeros = "0." + "0" * num_zeros
     price_suffix = pancake_price.split(zeros)[1][:11 - num_zeros]
+
+    market_cap = str(round(float(dagefather_data.market_cap)))
     price = {"prefix": zeros,
-             'suffix':  price_suffix}
+             "suffix":  price_suffix,
+             "market_cap": market_cap}
+
     web_site_url = {'url': current_site_url}
 
     # contruct context and render
